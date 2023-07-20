636507a58f68428b7a8b69bd03c66a058a06ae61
EM <eric.mann2393@gmail.com>
2023-07-20 15:59:32 -0400

    add auth store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex'
 import searches from './modules/searches/index.js'
+import auth from './modules/auth/index.js'
 
 export default createStore({
   modules: {
-    searches: searches
+    searches: searches,
+    auth: auth
   }
 })
