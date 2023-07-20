ad904ec3eee0a1dafc63b5db01838994276d080b
EM <eric.mann2393@gmail.com>
2023-07-20 16:59:11 -0400

    add login protections & state mgmt

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,12 +8,21 @@
       >
       </v-list-item>
       <v-list-item
-        prepend-icon="mdi-account"
+      v-if="!auth"
+        prepend-icon="mdi-login"
         title="Login"
         @click="$router.push('login')"
       ></v-list-item>
-      <v-list-item prepend-icon="mdi-bell" title="Notifications"></v-list-item>
+      <v-list-item v-if="auth" prepend-icon="mdi-account" title="Account" @click="$router.push('account')">
+      </v-list-item>
       <v-list-item
+        v-if="auth"
+        prepend-icon="mdi-bell"
+        title="Notifications"
+        @click="$router.push('notifications')"
+      ></v-list-item>
+      <v-list-item
+        v-if="auth"
         prepend-icon="mdi-logout"
         title="Logout"
         @click="logout"
@@ -22,6 +31,7 @@
     <v-divider />
     <v-list>
       <v-list-item
+      v-if="auth"
         prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
         title="Sandra Adams"
         subtitle="sandra_a88@gmailcom"
@@ -35,14 +45,20 @@
 export default {
   methods: {
     logout() {
-      if (localStorage.getItem("user-info")) {
+      let user = this.$store.getters["auth/auth"];
+      if (user) {
         localStorage.clear();
         this.$router.push({ name: "Login" });
-        this.$store.dispatch('auth/logout')
-        console.log('auth state: ', this.$store.getters['auth/auth'])
+        this.$store.dispatch("auth/logout");
+        console.log("auth state: ", this.$store.getters["auth/auth"]);
       } else {
         this.$router.push({ name: "SignUp" });
       }
+    },
+  },
+  computed: {
+    auth() {
+      return this.$store.getters["auth/auth"];
     },
   },
 };
