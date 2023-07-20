6cb92821b2f6ab992335ef70a91f05a219b8dcfe
EM <eric.mann2393@gmail.com>
2023-07-20 16:58:00 -0400

    add views, routing, & protection

---
+++ src/views/Account.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>Account Page for {{ name }}</h2>
+</template>
+
+<script>
+export default {
+  name: "Account",
+  data() {
+    return {
+      name: "",
+    };
+  },
+  methods: {
+    loadData() {
+      let user = this.$store.getters["auth/auth"];
+      if (!user) {
+        this.$router.push({ name: "SignUp" });
+      } else {
+        let data = JSON.parse(localStorage.getItem("user-info"));
+        this.name = data.charAt(0).toUpperCase() + data.slice(1);
+      }
+    },
+  },
+  mounted() {
+    this.loadData();
+  },
+};
+</script>
