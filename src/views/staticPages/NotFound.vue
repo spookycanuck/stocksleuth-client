01fb4c4ec4c9b8b43ba9d38788ae51d3b4d5bec6
EM <eric.mann2393@gmail.com>
2023-07-21 10:50:02 -0400

    add catch-all page for bad routing input

---
+++ src/views/staticPages/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>404 Error!</h1>
+    <h2>This page was not found</h2>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin-top: 10vh;
+  margin-left: 5vw;
+}
+h1 {
+  text-align: center;
+}
+</style>
