a47dee54aada03f2a027967530e911e9a073e6d2
EM <eric.mann2393@gmail.com>
2023-07-25 13:02:02 -0400

    formatting update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="container">
     <div>
-    <Search />
-  </div>
-  <div style="height: 100%; width:100%">
-    <h2 style="display:auto;">Welcome Back, {{ name }}</h2>
-    <v-card width="100%" height="100%">
-      <v-tabs v-model="tab" bg-color="error">
-        <v-tab value="summary">Summary</v-tab>
-        <v-tab value="price">Price</v-tab>
-        <v-tab value="articles">Articles</v-tab>
-      </v-tabs>
-      <v-card-text>
-        <v-window v-model="tab">
-          <v-window-item value="summary">
-            <Summary />
-          </v-window-item>
-          <v-window-item value="price">
-            <Price />
-          </v-window-item>
-          <v-window-item value="articles">
-            <Articles />
-          </v-window-item>
-        </v-window>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn block @click="search">Search</v-btn>
-      </v-card-actions>
-    </v-card>
-  </div>
+      <Search />
+    </div>
+    <div style="height: 100%; width: 100%">
+      <h2 style="display: auto">Welcome Back, {{ name }}</h2>
+      <v-card width="100%" height="100%">
+        <v-tabs v-model="tab" bg-color="error">
+          <v-tab value="summary">Summary</v-tab>
+          <v-tab value="price">Price</v-tab>
+          <v-tab value="articles">Articles</v-tab>
+        </v-tabs>
+        <v-card-text>
+          <v-window v-model="tab">
+            <v-window-item value="summary">
+              <Summary />
+            </v-window-item>
+            <v-window-item value="price">
+              <Price />
+            </v-window-item>
+            <v-window-item value="articles">
+              <Articles />
+            </v-window-item>
+          </v-window>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn block @click="search">Search</v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
   </div>
 </template>
 
@@ -45,14 +45,14 @@
 import Summary from "@/components/Summary.vue";
 import Price from "@/components/Price.vue";
 import Articles from "@/components/Articles.vue";
-import Search from "@/components/Search.vue"
+import Search from "@/components/Search.vue";
 
 export default {
   components: {
     Summary,
     Price,
     Articles,
-    Search
+    Search,
   },
   data() {
     return {
@@ -62,8 +62,8 @@ export default {
   },
   computed: {
     auth() {
-        return this.$store.getters["auth/auth"];
-      },
+      return this.$store.getters["auth/auth"];
+    },
   },
   methods: {
     search() {
@@ -94,9 +94,9 @@ export default {
     async loadData() {
       // If user info is not in local storage, user will not be
       //   able to go to this page & redirect to sign up
-      let user = localStorage.getItem('user-info')
+      let user = localStorage.getItem("user-info");
       if (user) {
-        let data = JSON.parse(localStorage.getItem('user-info'))
+        let data = JSON.parse(localStorage.getItem("user-info"));
         this.name = data.charAt(0).toUpperCase() + data.slice(1);
       }
     },
