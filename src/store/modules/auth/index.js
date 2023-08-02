a1badfec20ab463f7919d2e4ca3eef32b8a5df6c
EM <eric.mann2393@gmail.com>
2023-08-01 22:18:51 -0400

    remove comment

--- src/store/modules/auth/index.js
+++ src/store/modules/auth/index.js
@@ -6,11 +6,8 @@ export default {
     namespaced: true,
     state() {
         return {
-            auth: false, //Set to true for dev. Set to false for testing user workflow.
-            user: '' // Need to change this to a list and manipulate the payload
-                        // in the actions.js to pull from the API
-                        // also build out the dropbox for image and
-                        // add custom image to API
+            auth: true, //Set to true for dev. Set to false for testing user workflow.
+            user: ''
         }
     },
     actions,
