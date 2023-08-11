d231adac0a7b1e0146453851c59def79546a8af7
EM <eric.mann2393@gmail.com>
2023-08-11 14:51:51 -0400

    add stockList function from API, refactor newSearch to reduce code

--- src/store/modules/searches/actions.js
+++ src/store/modules/searches/actions.js
@@ -1,83 +1,41 @@
 import axios from "axios";
 
 export default {
+  async getStockList(context) {
+    let res;
+    var stockList = JSON.parse(localStorage.getItem('stockList'))
+    if (!stockList) {
+      res = await axios
+      .get(`http://localhost:5000/api/v1/stock-list`);
+      if (res.status === 200 ) {
+        stockList = res.data
+      }
+    }
+    context.commit("setStockList", stockList)
+  },
   async newSearch(context, payload) {
     const ticker = payload.toUpperCase();
     context.commit("setTicker", payload);
-    let recentPriceData;
-    let graphData;
-    let res;
-    let res2;
+    let search;
+    let price;
+    let overview;
 
     try {
-      res = await axios
+      price = await axios
         .get(`http://localhost:5000/api/v1/price?ticker=${ticker}`);
-      res2 = await axios
+      overview = await axios
         .get(`http://localhost:5000/api/v1/overview?ticker=${ticker}`);
-      if (res.status == 200 && res.data.length > 0) {
-        let x = res.data[0];
-        let y = res2.data;
-        graphData = res.data;
-        // TODO: Map entire ticker history -- X will only return the latest date
-        //    graphData is the entire API data call, and sends it to another action
-        recentPriceData = {
-          id: ticker,
-          adjClose: x.adjClose,
-          change: x.change,
-          changeOverTime: x.changeOverTime,
-          changePercent: x.changePercent,
-          close: x.close,
-          date: x.date,
-          high: x.high,
-          label: x.label.slice(0, -2) + "20" + x.label.slice(-2),
-          low: x.low,
-          open: x.open,
-          unadjustedVolume: x.unadjustedVolume,
-          volume: x.volume,
-          vwap: x.vwap,
-          address: y.address,
-          beta: y.beta,
-          ceo: y.ceo,
-          changes: y.changes,
-          cik: y.cik,
-          city: y.city,
-          name: y.companyName,
-          country: y.country,
-          currency: y.currency,
-          cusip: y.cusip,
-          dcf: y.dcf,
-          dcfDiff: y.dcfDiff,
-          defaultImage: y.defaultImage,
-          description: y.description,
-          exchange: y.exchange,
-          exchangeAbbv: y.exchangeShortName,
-          employees: y.fullTimeEmployees,
-          image: y.image,
-          industry: y.industry,
-          ipoDate: y.ipoDate,
-          isTrading: y.isActivelyTrading,
-          isAdr: y.isAdr,
-          isEtf: y.isEtf,
-          isFund: y.isFund,
-          isin: y.isin,
-          lastDiv: y.lastDiv,
-          mktCap: y.mktCap,
-          phone: y.phone,
-          priceRange: y.range,
-          sector: y.sector,
-          state: y.state,
-          symbol: y.symbol,
-          volAvg: y.volAvg,
-          website: y.website,
-          zip: y.zip,
-        };
+      if (price.status == 200 && price.data.length > 0) {
+        search = {
+          ...price.data[0],
+          ...overview.data,
+          data: price.data
+        }
       }
-      // console.log(Object.keys(recentPriceData).length)
-      context.commit("addSearch", recentPriceData);
+      context.commit("addSearch", search);
       context.commit("failed", null);
-      context.dispatch("buildGraph", graphData);
       if (context.getters.currentSearch == null) {
-        context.commit("currentSearch", recentPriceData);
+        context.commit("currentSearch", search);
       }
     } catch (e) {
       context.commit("failed", true);
@@ -89,7 +47,7 @@ export default {
     //    buildGraph takes the API data from newSearch and should structure the data
     //    appropriately for the graph in Graph.vue
     context.commit("buildGraph", payload);
-    // console.log("buildGraph: ", payload);
+    console.log("buildGraph: ", payload);
   },
   currentSearch(context, payload) {
     context.commit("currentSearch", payload);
